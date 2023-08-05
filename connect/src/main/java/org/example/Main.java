6a3a8d492948c869ab659179aea3e2a041a03206
Jeevasimba <96039278+Jeevasimba@users.noreply.github.com>
2023-08-05 10:15:42 +0530

    Add files via upload

---
+++ connect/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+
+    static String c;
+    public static void main(String[] args) {
+       c = "hai";
+       while (true){
+
+       }
+    }
+
+    public String geta(){
+        return c;
+    }
+}
---
+++ javafx/src/main/java/org/p/Main.java
@@ -0,0 +1,8 @@
+package org.p;
+
+public class Main {
+    public static void main(String[] args) {
+        org.example.Main a = new org.example.Main();
+        System.out.println(a.geta());
+    }
+}
